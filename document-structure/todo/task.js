edbc3157e92347f974f3661a678a22b58b30b3cd
Kopylov Sergey <kopylov.serge@gmail.com>
2022-04-27 10:06:03 +0300

    ДЗ к лекции «Изменение структуры HTML-документа» Правки 1.

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,31 +1,55 @@
 const form = document.getElementById('tasks__form');
-form.addEventListener('submit', function(e) {
-// чтобы форма не отправлялась и страничка не перегружалась
-    e.preventDefault();
-}, false);
-
-
 const div = document.getElementById('tasks__list');
 const input = document.getElementById('task__input');
 
-function addClose(e) {
-    if ((e.code === "Enter" || e.code === "NumpadEnter") && input.value) {
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    addClose();
+});
+
+function addClose() {
+    if (input.value.trim()) {
         div.insertAdjacentHTML('beforeend', '<div class="task"><div class="task__title">'+ input.value +'</div><a href="#" class="task__remove">&times;</a></div>');
 
         const divClose = Array.from(document.querySelectorAll("div.task"));
-        divClose.forEach((el) => {
-            const divTitle = el.querySelector(".task__title");
-            const close = el.querySelector(".task__remove");
-            if (divTitle.textContent === input.value) {
-                close.addEventListener('click', (e) => {
-                    el.remove();
-                    return false;
-                })
-            }
-        });
+        const endEl = divClose[divClose.length - 1];
+        const close = endEl.querySelector(".task__remove");
+        close.addEventListener('click', () => {
+            endEl.remove();
+            return false;
+        })
 
         input.value = "";
     }
 }
 
-input.addEventListener('keydown', addClose);
+
+
+
+// const form = document.getElementById('tasks__form');
+// form.addEventListener('submit', function(e) {
+// // чтобы форма не отправлялась и страничка не перегружалась
+//     e.preventDefault();
+// }, false);
+
+
+// const div = document.getElementById('tasks__list');
+// const input = document.getElementById('task__input');
+
+// function addClose(e) {
+//     if ((e.code === "Enter" || e.code === "NumpadEnter") && input.value.trim()) {
+//         div.insertAdjacentHTML('beforeend', '<div class="task"><div class="task__title">'+ input.value +'</div><a href="#" class="task__remove">&times;</a></div>');
+
+//         const divClose = Array.from(document.querySelectorAll("div.task"));
+//         const endEl = divClose[divClose.length - 1];
+//         const close = endEl.querySelector(".task__remove");
+//         close.addEventListener('click', () => {
+//             endEl.remove();
+//             return false;
+//         })
+
+//         input.value = "";
+//     }
+// }
+
+// input.addEventListener('keydown', addClose);
